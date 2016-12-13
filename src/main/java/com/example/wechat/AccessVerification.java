161522add7676288208d5a109bcb4b55c6294b46
changyulin <cjdxchangyulin@163.com>
2016-12-13 23:19:29 +0800

    add postdata

--- src/main/java/com/example/wechat/AccessVerification.java
+++ src/main/java/com/example/wechat/AccessVerification.java
@@ -1,15 +1,21 @@
 package com.example.wechat;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 public class AccessVerification extends HttpServlet {
-    
+
 	/**
 	 * 确认请求来自微信服务器
 	 */
@@ -41,6 +47,39 @@ public class AccessVerification extends HttpServlet {
 	 * 处理微信服务器发来的消息
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO 消息的接收、处理、响应
+		try {
+			Map<String, String> dataMap = parseXml(request);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
+	public static Map<String, String> parseXml(HttpServletRequest request) throws Exception {
+		// 将解析结果存储在HashMap中
+		Map<String, String> map = new HashMap<String, String>();
+
+		// 从request中取得输入流
+		InputStream inputStream = request.getInputStream();
+		// 读取输入流
+		/*SAXReader reader = new SAXReader();
+		Document document = reader.read(inputStream);
+		System.out.println("post data:");
+		System.out.println(document.asXML());
+		// 得到xml根元素
+		Element root = document.getRootElement();
+		// 得到根元素的所有子节点
+		List<Element> elementList = root.elements();
+
+		// 遍历所有子节点
+		for (Element e : elementList)
+			map.put(e.getName(), e.getText());*/
+
+		// 释放资源
+		inputStream.close();
+		inputStream = null;
+
+		return map;
+	}
+
 }
